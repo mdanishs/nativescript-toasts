52bbbeae887d403ae3c4df8384d98aa6e7836417
M Danish Siddiqui <mohammaddsiddiqui@gmail.com>
2016-09-29 01:39:33 +0500

    Added plugin code

---
+++ index.js
@@ -0,0 +1,41 @@
+var app = require("application");
+var frameModule = require("ui/frame");
+
+
+/**
+ * The time duration for which toast will be displayed on the screen
+ * For Android the values will be mapped to Toast.LENGTH_LONG and Toast.LENGTH_SHORT
+ * For iOS the values will be mapped to the numeric values of Android enum which are 3.5 for LONG and 2 for SHORT
+ */
+const DURATION = {
+  LONG : "long",
+  SHORT : "short"
+}
+exports.DURATION = DURATION;
+
+/**
+ * show - displays the toast for iOS and Android
+ *
+ * @param  {object} toastObject javascript object with properties text and duration. Duration by default is short for android.
+ * @return {void}
+ */
+exports.show = function(toastObject){
+    var duration;
+  if(app.ios){
+    duration = 2.0;
+    if(toastObject.duration === DURATION.LONG){
+        duration = 3.5;
+    }
+    CSToastManager.setDefaultDuration(duration);
+    frameModule.topmost().ios.controller.view.makeToast(toastObject.text);
+
+  }else if(app.android){
+    const Toast = android.widget.Toast;
+    duration = Toast.LENGTH_SHORT;
+    if(toastObject.duration === DURATION.LONG){
+      duration = Toast.LENGTH_LONG;
+    }
+
+    Toast.makeText(app.android.context, toastObject.text, duration).show();
+  }
+}
