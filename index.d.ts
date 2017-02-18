a3df4b71f0324d7ff10ab77ebeef7fb3000889fc
M Danish Siddiqui <mohammaddsiddiqui@gmail.com>
2017-02-18 19:27:33 +0500

    updated typescript declarations and version number

--- index.d.ts
+++ index.d.ts
@@ -7,9 +7,16 @@ export class DURATION {
     static LONG:string;
 }
 
+export class POSITION {
+    static TOP:string;
+    static CENTER:string;
+    static BOTTOM:string;
+}
+
 export interface ToastOptions {
     text: string;
     duration: DURATION;
+    position?: POSITION
 }
 
 export function show(toastObject: ToastOptions)
